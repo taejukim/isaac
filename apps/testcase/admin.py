616c90c0efe1ac14a7369c492a2b36f1d071bc33
김태주/클라우드QA파트/TO <taeju.kim@nhntoast.com>
2020-11-09 17:15:20 +0900

    Testcase admin page 설정

--- apps/testcase/admin.py
+++ apps/testcase/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.testcase.models import Service, Function, Testcase, Procedure\
+    ,Region, Testcase_history, Procedure_history
 
-# Register your models here.
+admin.site.register(Service)
+admin.site.register(Function)
+admin.site.register(Testcase)
+admin.site.register(Procedure)
+admin.site.register(Region)
+admin.site.register(Testcase_history)
+admin.site.register(Procedure_history)
