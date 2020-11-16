e6865b12afb748015e750c2256602caec374022c
김태주/클라우드QA파트/TO <taeju.kim@nhntoast.com>
2020-11-16 22:51:56 +0900

    change staticfile dirs

--- isaac_project/settings.py
+++ isaac_project/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+# STATIC_ROOT = BASE_DIR / 'static/'
 
-# STATICFILES_DIRS = (
-#     BASE_DIR / 'static/',
-#     BASE_DIR / 'static/files'
-# )
+STATICFILES_DIRS = (
+    BASE_DIR / 'static/',
+    BASE_DIR / 'static/files'
+)
