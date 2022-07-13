5bcd8a19899d3dc800b19067c26d81116d0fac5c
taeju.kim <iam@taeju.kim>
2022-07-13 09:30:07 +0900

    table 변경

--- apps/resources/admin.py
+++ apps/resources/admin.py
@@ -1,21 +1,8 @@
 from django.contrib import admin
-from apps.resources.models import ServiceInfo, Resource, \
-    Service, Member, WorkHistory, Category
+from apps.resources.models import ServiceInfo, Member, WorkHistory, Category
 
-
-# @admin.register(ServiceInfo)
-# class ServiceInfoAdmin(admin.ModelAdmin):
-#     search_fields = ('category', 'name_detail', )
-#     list_display=['id', 'service_no', 'sort', 'category',
-#         'name_detail','priority', 'group', 'team_member', 'create_dt', ]
-
-# @admin.register(Resource)
-# class ResourceAdmin(admin.ModelAdmin):
-#     search_fields = ('category', 'service_no', 'name_detail',)
-#     list_display = ['id', 'service_no', 'mm', 'month', 'year', 'category', 'name_detail', 'create_dt']
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
+@admin.register(ServiceInfo)
+class ServiceInfoAdmin(admin.ModelAdmin):
     list_display = ['service', 'category', 'priority','man_month','ordering']
 
 @admin.register(Member)
--- apps/resources/models.py
+++ apps/resources/models.py
@@ -1,47 +1,5 @@
 from django.db import models
 
-class ServiceInfo(models.Model):
-    id = models.AutoField(primary_key=True)
-    service_no = models.IntegerField(unique=True, verbose_name='서비스 이름')
-    sort = models.IntegerField(verbose_name='정렬을 위한 no')
-    category = models.CharField(max_length=50, blank=True, null=True, verbose_name='분류')
-    name_detail = models.CharField(max_length=50, blank=True, null=True, verbose_name='서비스 상세')
-    priority = models.CharField(max_length=3, blank=True, null=True, verbose_name='서비스 우선순위')
-    group = models.CharField(max_length=20, blank=True, null=True, verbose_name='M매뉴얼')
-    release_yn = models.CharField(max_length=5, blank=True, null=True, verbose_name='출시여부')
-    d_type = models.CharField(max_length=10, blank=True, null=True)
-    standard_mm = models.FloatField(verbose_name='기준리소스')
-    standard_mm_dpl = models.FloatField(verbose_name='배포기준고려')
-    note_service = models.CharField(max_length=200, blank=True, null=True)
-    part = models.CharField(max_length=10, blank=True, null=True)
-    team_member = models.CharField(max_length=30, blank=True, null=True)
-    dpl_cycle = models.CharField(max_length=10, blank=True, null=True)
-    create_dt = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.service_no)
-    
-
-class Resource(models.Model):
-    id = models.AutoField(primary_key=True)
-    service_no = models.ForeignKey(ServiceInfo, to_field='service_no', on_delete=models.CASCADE)
-    mm = models.FloatField()
-    month = models.IntegerField(null=False)
-    year = models.IntegerField()
-    category = models.CharField(max_length=50, blank=True, null=True, verbose_name='서비스명')
-    name_detail = models.CharField(max_length=50, blank=True, null=True, verbose_name='서비스명 상세')
-    group = models.CharField(max_length=10, blank=True, null=True, verbose_name='M매뉴얼 자동화 공공')
-    md = models.BigIntegerField()
-    translation = models.FloatField(verbose_name='추이')
-    note_resource = models.CharField(max_length=200, blank=True, null=True)
-    part = models.CharField(max_length=10, blank=True, null=True)
-    team_member = models.CharField(max_length=10, blank=True, null=True)
-    create_dt = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.service_no)
-    
-
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=100, null=False, blank=False, verbose_name='카테고리 이름')
@@ -49,7 +7,7 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
-class Service(models.Model):
+class ServiceInfo(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.ForeignKey(Category, verbose_name="카테고리", on_delete=models.CASCADE)
     service = models.CharField(max_length=100, null=False, blank=False, verbose_name='서비스 이름')
@@ -73,7 +31,7 @@ class Member(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False, verbose_name='이메일')
     title = models.CharField(max_length=10, null=False, blank=False, verbose_name='직함')
     position = models.CharField(max_length=10, null=False, blank=False, verbose_name='직책')
-    services = models.ManyToManyField(Service, blank=True,
+    services = models.ManyToManyField(ServiceInfo, blank=True,
      related_name='services', verbose_name='담당 서비스')
     description = models.TextField(null=True, blank=True, verbose_name='비고')
     updated_at = models.DateTimeField(auto_now=True)
@@ -99,7 +57,7 @@ class WorkHistory(models.Model):
     )
     
     id = models.AutoField(primary_key=True)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    service = models.ForeignKey(ServiceInfo, on_delete=models.CASCADE)
     start_date = models.DateField(verbose_name='시작 날짜')
     end_datet = models.DateField(verbose_name='종료 날짜')
     work_type = models.CharField(max_length=20, null=False, blank=False,
